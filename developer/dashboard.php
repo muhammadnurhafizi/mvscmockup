7d58f685c55b732b2a8a28226de220ae08288059
Muhammad Nurhafizi <muhammadnurhafizi3012@gmail.com>
2022-03-07 15:27:56 +0800

    add dev dashboard

--- developer/dashboard.php
+++ developer/dashboard.php
@@ -11,6 +11,89 @@
 </head>
 <body>
 
+    <div class="container">
+        <div class="container-fluid">
+
+            <div class="row pt-5">
+                <div class="col-9">
+
+                </div>
+                <div class="col-3">
+                    <button class="btn btn-primary">Back to Application Status Page</button>
+                </div>
+            </div>
+
+            <div class="row mt-3">
+                <div class="col-12">
+                    <div class="card">
+                        <div class="card-header">
+                            <h3 class="card-title">My Project Request</h3>
+                        </div>
+                        <div class="card-body">
+                            <div class="row">
+                                <div class="col-2"></div>
+                                <div class="col-2">
+                                    ferfgerfs
+                                </div>
+                                <div class="col-2">
+
+                                </div>
+                                <div class="col-2">
+
+                                </div>
+                                <div class="col-2">
+
+                                </div>
+                                <div class="col-2"></div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="row mt-3">
+                <div class="col-12">
+                    <div class="card">
+                        <div class="card-header">
+                            <h3 class="card-title">My Project Status</h3>
+                        </div>
+                        <div class="card-body">
+                            <div class="row">
+                                <div class="col-12">
+                                    <table class="table table-bordered">
+                                        <thead>
+                                            <tr>
+                                                <th></th>
+                                                <th></th>
+                                                <th></th>
+                                                <th></th>
+                                                <th></th>
+                                                <th></th>
+                                            </tr>
+                                        </thead>
+                                        <tbody>
+                                            <tr>
+                                                <td>
+
+                                                </td>
+                                                <td></td>
+                                                <td></td>
+                                                <td></td>
+                                                <td></td>
+                                                <td></td>
+                                            </tr>
+                                        </tbody>
+                                    </table>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+    </div>
+
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
 </body>
 </html>
