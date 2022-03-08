742159d4d07b64825052a749efd8437571145ac6
Mohd Aminuddin <68073525+mhdamn34@users.noreply.github.com>
2022-03-08 09:04:34 +0800

    push technical_review

--- developer/technical_review.php
+++ developer/technical_review.php
@@ -6,11 +6,147 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    
+   
 
     <title>Technical Review</title>
 </head>
 <body>
+    
+<div class="container">
+    <div class="container-fluid">
+
+
+        <div class="row pt-5">
+            <div class="row">
+                <div class="row">
+                    <h1>Project Management</h1>
+                </div>
+                <div class="row pt-1">
+                    <p>Keep up to date with the status</p>
+                </div>
+                <div class="row">
+                    <p>Document Status</p>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col"></div>
+                <div class="col-md-auto">
+                    <button class="btn btn-primary">Back to Application Status Page</button>
+                </div>
+            </div>
+            
+        </div>
+
+        <div class="row mt-3">
+            <div class="col-12">
+                <div class="card">
+                    <div class="card-header">Status: Technical Document</div>
+
+                    <div class="card-body">
+                        <div class="row">
+
+                            <div class="col-4">
+                                <button class="btn btn-primary">Upload Site Progress Photo</button>
+                            </div>
+                            <div class="col-4">
+                                <button class="btn btn-primary">Upload Supply Required Date</button>
+                            </div>
+                            <div class="col-4">
+                                <button class="btn btn-primary">Schedule Appointment</button>
+                            </div>
+                        </div>
+
+                        <div class="row" name="graphic"></div>
+
+                        <div class="row">
+                            <div class="col-4">Technical Documents Submission</div>
+                            <div class="col-4">Technical Review</div>
+                            <div class="col-4">Connection Charges</div>
+                        </div>
+                    </div>
+                </div>
+                
+            </div>
+        </div>
+
+
+        <div class="row mt-3">
+            <div class="col-12">
+                <div class="card">
+                    <div class="card-body">
+                        <div class="row">
+                            <div class="col">Tetuan:</div>
+                            <div class="col">SRD:</div>
+                        </div>
+
+                        <div class="row">
+                            <div class="col">Description:</div>
+                            <div class="col">Project No:</div>
+                        </div>
+
+                        <div class="row">
+                            <div class="col">MD:</div>
+                            <div class="col">Developer:</div>
+                        </div>
+
+                        <div class="row">
+                            <div class="col">Last Updated On:</div>
+                            <div class="col">Consultant:</div>
+                        </div>
+                    </div>    
+                </div>
+            </div>
+        </div>
+
+    
+        <div class="row mt-3">
+                <div class="col-12">
+                    <div class="card">
+                        <div class="card-header">
+                            <h3 class="card-title">My Project Status</h3>
+                        </div>
+                        <div class="card-body">
+                            <div class="row">
+                                <div class="col-12">
+                                    <table class="table table-bordered">
+                                        <thead>
+                                            <tr>
+                                                <th>Document Names</th>
+                                                <th>Status</th>
+                                                <th>Submission</th>
+                                                <th>Submission Date</th>
+                                                <th>Status</th>
+                                            </tr>
+                                        </thead>
+                                        <tbody>
+                                            <tr>
+                                                <td>
+
+                                                </td>
+                                                <td></td>
+                                                <td></td>
+                                                <td></td>
+                                                <td></td>
+                                                <td></td>
+                                            </tr>
+                                        </tbody>
+                                    </table>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+        </div>
+
+
+
+    </div>
+</div>
+
+
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+    
 </body>
 </html>
